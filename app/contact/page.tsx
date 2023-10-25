681ee93809802433345c143616c9e3a9c04cb16c
Linz Pierre <pierre.linz@orange.fr>
2023-10-25 12:24:26 +0200

    test

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -94,7 +94,7 @@ export default function Contact() {
       {isSubmitted ? (
         <p className="text-green-500 mt-4">{isSubmitted}</p>
       ) : (
-        <p></p>
+        <p className="text-red-500 mt-4">Complete the form pls</p>
       )}
     </main>
   );
